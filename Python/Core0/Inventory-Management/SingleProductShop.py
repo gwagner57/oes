87ea1556ae153e6b440a83d237209a2d64e7b398
OsamaAl-Wardi <osamaalwardi@yahoo.com>
2020-11-17 23:01:19 +0100

    fixed the import issue with SingleProductShop.py and replaced the hyphen from OES-Foundations with an underscore

--- Python/Core0/Inventory-Management/SingleProductShop.py
+++ Python/Core0/Inventory-Management/SingleProductShop.py
@@ -1,11 +1,16 @@
-import importlib
-mod = importlib.import_module(".OESFoundations", "Core0.OESpyCore0") 
+import sys, os
+
+module_path = os.path.abspath('OESpy-Core0/')
+sys.path.insert(1, module_path)
+import OES_Foundations
 
 
-class SingleProductShop(mod.oBJECT):
+class SingleProductShop(OES_Foundations.oBJECT):
     def __init__(self, id, name, quantityInStock, reorderLevel, targetInventory, labels):
         super().__init__(id, name)
         self.quantityInStock = quantityInStock
         self.reorderLevel = reorderLevel
         self.targetInventory = targetInventory
         self.labels = labels
+
+#print(module_path)
