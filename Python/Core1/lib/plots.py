10071861647ddea043f166ec94173e8c6757a107
Fathima Shaik <shaikfat@b-tu.de>
2020-11-20 15:17:40 +0100

    Implemented the changes in the rand.py
    Implemented all the changes mentioned in the email.

--- Python/Core1/lib/plots.py
+++ Python/Core1/lib/plots.py
@@ -2,19 +2,19 @@ import matplotlib.pyplot as plt
 import sys
 if not sys.warnoptions:
     import warnings
-    warnings.simplefilter("ignore")
+    warnings.simplefilter( "ignore" )
 
 import seaborn as sns
 # settings for seaborn plotting style
-sns.set(color_codes=True)
+sns.set( color_codes=True )
 # settings for seaborn plot sizes
-sns.set(rc={'figure.figsize':(5,5)})
+sns.set( rc={'figure.figsize':(5,5)} )
 
-def make_plot(name, data):
-   ax = sns.distplot(data,
+def make_plot( name, data ):
+   ax = sns.distplot( data,
       kde=True,
       bins=100,
       color='skyblue',
-      hist_kws={"linewidth": 15,'alpha':1})
-   ax.set(xlabel=name, ylabel='Frequency')
+      hist_kws={"linewidth": 15,'alpha':1} )
+   ax.set( xlabel=name, ylabel='Frequency')
    plt.show()
--- Python/Core1/lib/test.py
+++ Python/Core1/lib/test.py
@@ -7,35 +7,35 @@ import plots as plt
 rand = rnd.Rand()
 
 exponential_value = rand.exponential(0.5)
-plt.make_plot("Exponential", exponential_value)
+plt.make_plot( "Exponential", exponential_value )
 
 
-gamma1_value = rand.gamma1(1.0, 2.0)
-plt.make_plot("Gamma", gamma1_value)
+gamma_value = rand.gamma(1.0, 2.0)
+plt.make_plot( "Gamma", gamma_value )
 
 
 normal_value = rand.normal(1.5, 0.5)
-plt.make_plot("Normal", normal_value)
+plt.make_plot( "Normal", normal_value )
 
-uniform1_value = rand.uniform1(0.5, 1.5)
-plt.make_plot("Uniform", uniform1_value)
+uniform_value = rand.uniform(0.5, 1.5)
+plt.make_plot( "Uniform", uniform_value )
 
 
-uniformInt_value = rand.uniformInt(1, 6)
-plt.make_plot("UniformInt", uniformInt_value)
+uniformInt_value = rand.uniformInt(1, 40)
+plt.make_plot( "UniformInt", uniformInt_value )
 
 triangular_value = rand.triangular(0.5, 1.5, 1.0)
-plt.make_plot("Triangular", triangular_value)
+plt.make_plot( "Triangular", triangular_value )
 
-pareto1_value = rand.pareto1(2.0)
-plt.make_plot("Pareto", pareto1_value)
+pareto_value = rand.pareto(2.0)
+plt.make_plot( "Pareto", pareto_value )
 
 
-weibull1_value = rand.weibull1(1,0.5)
-plt.make_plot("Weidbull", weibull1_value)
+weibull_value = rand.weibull(1,0.5)
+plt.make_plot( "Weibull", weibull_value )
 
-frequency1_value = rand.frequency1({"20":1.4, "30":2.6 , "60":3.8, "80":1.9})
-plt.make_plot("Frequency", frequency1_value)
+frequency_value = rand.frequency({"20":1.4, "30":2.6 , "60":3.8, "80":1.9})
+plt.make_plot( "Frequency", frequency_value )
 
-pert_value = rand.pertdist1(10, 190, 200)
-plt.make_plot("pertdistributiion", pert_value)
+pert_value = rand.pertdist(10, 190, 200)
+plt.make_plot( "PertDistributiion", pert_value )
