5d155e686c09ce0be8a6ed5807af35ce415755e1
Fathima Shaik <shaikfat@b-tu.de>
2020-11-29 20:14:31 +0100

    Made seed value optional and integrated the test of the module
    Implemented all the remarks mentioned on the email: seev value optional, changed variable names and made an integrated the test of the module "oes_distributions.py" within the module file

--- Python/Core1/lib/oes_distributions.py
+++ Python/Core1/lib/oes_distributions.py
@@ -27,42 +27,66 @@ sns.set( color_codes=True )
 sns.set( rc = { 'figure.figsize':(5,5) } )
 
 class Rand:
-   
-   def __init__( self, seed_value = 42):
+   seed = None
+   def __init__( self, seed_value = None):
+      if(seed_value is not None):
          self.seed = seed_value
-         # self.seed = random.seed( seed_value )
          print("seed_value: ", self.seed)
       
    def exponential( self, lambda1 ):
-      data_expon = expon.rvs ( scale = 1/lambda1, loc = 0, size = 1000, random_state = self.seed )
+      if(self.seed is not None):
+         data_expon = expon.rvs ( scale = 1/lambda1, loc = 0, size = 1000, random_state = self.seed )
+      else:
+         data_expon = expon.rvs ( scale = 1/lambda1, loc = 0, size = 1000 )
       return data_expon
 
    def gamma( self, shape, scale ):
-      data_gamma = gamma.rvs ( a = shape, scale = scale, size = 10000, random_state = self.seed )
+      if(self.seed is not None):
+         data_gamma = gamma.rvs ( a = shape, scale = scale, size = 10000, random_state = self.seed )
+      else:
+         data_gamma = gamma.rvs ( a = shape, scale = scale, size = 10000 )
       return data_gamma
 
    def normal( self, mean, stdDev ):
-      data_normal = norm.rvs ( size = 10000, loc = mean, scale = stdDev, random_state = self.seed )
+      if(self.seed is not None):
+         data_normal = norm.rvs ( size = 10000, loc = mean, scale = stdDev, random_state = self.seed )
+      else:
+         data_normal = norm.rvs ( size = 10000, loc = mean, scale = stdDev )
       return data_normal
 
    def uniform( self, lowerBound, upperBound ):
-      data_uniform = uniform.rvs ( size = 10000, loc = lowerBound, scale = upperBound, random_state = self.seed )
+      if(self.seed is not None):
+         data_uniform = uniform.rvs ( size = 10000, loc = lowerBound, scale = upperBound, random_state = self.seed )
+      else:
+         data_uniform = uniform.rvs ( size = 10000, loc = lowerBound, scale = upperBound )
       return data_uniform
 
    def uniformInt( self,lowerBound, upperBound ):
-      data_uniformInt = randint.rvs ( high = upperBound, low = lowerBound , loc = 0, size = 1000, random_state = self.seed )
+      if(self.seed is not None):
+         data_uniformInt = randint.rvs ( high = upperBound, low = lowerBound , loc = 0, size = 1000, random_state = self.seed )
+      else:
+         data_uniformInt = randint.rvs ( high = upperBound, low = lowerBound , loc = 0, size = 1000)
       return data_uniformInt
 
    def triangular( self, lowerBound, upperBound, mode ):
-      data_triangular = triang.rvs ( size = 10000, c = mode, loc = lowerBound, scale = upperBound, random_state = self.seed )
+      if(self.seed is not None):
+         data_triangular = triang.rvs ( size = 10000, c = mode, loc = lowerBound, scale = upperBound, random_state = self.seed )
+      else:
+         data_triangular = triang.rvs ( size = 10000, c = mode, loc = lowerBound, scale = upperBound )
       return data_triangular
 
    def pareto( self, shape ):
-      data_pareto  = pareto.rvs ( size = 10000, b = shape, random_state = self.seed )
+      if(self.seed is not None):
+         data_pareto  = pareto.rvs ( size = 10000, b = shape, random_state = self.seed )
+      else:
+         data_pareto  = pareto.rvs ( size = 10000, b = shape )
       return data_pareto
 
    def weibull( self,shape,scale ):
-      data_weibull = weibull_max.rvs( c = shape, scale = scale, size = 10000, random_state = self.seed  )
+      if(self.seed is not None):
+         data_weibull = weibull_max.rvs( c = shape, scale = scale, size = 10000, random_state = self.seed  )
+      else:
+         data_weibull = weibull_max.rvs( c = shape, scale = scale, size = 10000  )
       return data_weibull
    
    def frequency( self, frequencyMap ):
@@ -76,6 +100,10 @@ class Rand:
 
    def pertdist( self, minimum, most_likely, maximum ):
       pert = PERT( minimum, most_likely, maximum )
-      pert = sns.kdeplot( pert.rvs( 10000, random_state = self.seed ) )
+      if(self.seed is not None):
+         pert = sns.kdeplot( pert.rvs( 10000, random_state = self.seed ) )
+      else:
+         pert = sns.kdeplot( pert.rvs( 10000) )
+
 
 
--- Python/Core1/lib/test.py
+++ Python/Core1/lib/test.py
@@ -5,39 +5,47 @@ from oes_distributions import *
 #importlib.import_module("rand")
 from plots import make_plot
 
-seed = Rand( random.randint( 1,1000 ) )
+def test(seed):
+	# seed = random.randint( 1,1000 )
+	rnd = Rand( seed )
 
-#print(seed.seed)
+	exponential_value = rnd.exponential( 0.5 )
+	make_plot( "Exponential", exponential_value )
 
-exponential_value = seed.exponential( 0.5 )
-make_plot( "Exponential", exponential_value )
+	gamma_value = rnd.gamma( 1.0, 2.0 )
+	make_plot( "Gamma", gamma_value )
+
+	normal_value = rnd.normal( 1.5, 0.5 )
+	make_plot( "Normal", normal_value )
+
+	uniform_value = rnd.uniform( 0.5, 1.5 )
+	make_plot( "Uniform", uniform_value )
+
+	uniformInt_value = rnd.uniformInt( 1, 40 )
+	make_plot( "UniformInt", uniformInt_value )
+
+	triangular_value = rnd.triangular( 0.5, 1.5, 1.0 )
+	make_plot( "Triangular", triangular_value )
+
+	pareto_value = rnd.pareto( 2.0 )
+	make_plot( "Pareto", pareto_value )
+
+	weibull_value = rnd.weibull( 1, 0.5 )
+	make_plot( "Weibull", weibull_value )
+
+	frequency_value = rnd.frequency( { "20":1.4, "30":2.6 , "60":3.8, "80":1.9 } )
+	make_plot( "Frequency", frequency_value )
+
+	pert_value = rnd.pertdist( 10, 190, 200 )
+	make_plot( "PertDistributiion", pert_value )
 
 
-gamma_value = seed.gamma( 1.0, 2.0 )
-make_plot( "Gamma", gamma_value )
-
-
-normal_value = seed.normal( 1.5, 0.5 )
-make_plot( "Normal", normal_value )
-
-uniform_value = seed.uniform( 0.5, 1.5 )
-make_plot( "Uniform", uniform_value )
-
-uniformInt_value = seed.uniformInt( 1, 40 )
-make_plot( "UniformInt", uniformInt_value )
-
-triangular_value = seed.triangular( 0.5, 1.5, 1.0 )
-make_plot( "Triangular", triangular_value )
-
-pareto_value = seed.pareto( 2.0 )
-make_plot( "Pareto", pareto_value )
-
-
-weibull_value = seed.weibull( 1, 0.5 )
-make_plot( "Weibull", weibull_value )
-
-frequency_value = seed.frequency( { "20":1.4, "30":2.6 , "60":3.8, "80":1.9 } )
-make_plot( "Frequency", frequency_value )
-
-pert_value = seed.pertdist( 10, 190, 200 )
-make_plot( "PertDistributiion", pert_value )
+if __name__ == "__main__":
+    import sys
+    seed = None
+    if(len(sys.argv) == 2):
+    	try:
+    		seed = int(sys.argv[1])
+    	except:
+    		print("An exception occurred")
+    test(seed)
