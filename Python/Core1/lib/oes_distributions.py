02d18944de3de1558180a6bfd57aac963eff979b
Fathima Shaik <shaikfat@b-tu.de>
2020-12-03 00:07:03 +0100

    Implemented All the distribution functions except  for UniformInt
    except the uniform int all other distributions are implemented.

--- Python/Core1/lib/oes_distributions.py
+++ Python/Core1/lib/oes_distributions.py
@@ -1,5 +1,5 @@
 
-# still working need to implement Frequency , gamma , exponential  
+# still working need to implement UniformInt 
 
 """
 Create random number streams
@@ -17,7 +17,7 @@ rns2_2 = gen2.rand()
 
 from numpy.random import default_rng
 from pert import PERT
-from scipy.stats import randint
+from scipy import stats
 import plots 
 import matplotlib.pyplot as plt
 import sys
@@ -35,111 +35,65 @@ class Rand:
    def __init__( self, seed ):
       self.rng = default_rng( seed )  # RNG initialization
 
+   def exponential( self, event_rate, size ):
+      return  self.rng.exponential( scale = 1 / event_rate, size = 10000  )
 
-   def exponential(  self, event_rate ):
-      return  self.rng.exponential( scale = 1 / event_rate )
-
-
-   # def gamma( self, shape , scale,  ):
-   #  return self.rng.gamma ( a = shape , scale =  )
-      # if(self.seed is not None):
-      #    data_gamma = gamma.rvs ( a = shape, scale = scale, size = 10000, random_state = self.seed )
-      # else:
-      #    data_gamma = gamma.rvs ( a = shape, scale = scale, size = 10000 )
-      # return data_gamma
+   def gamma( self, alpha, beta, size ):
+    return self.rng.gamma ( shape = alpha , scale = beta , size = 10000 )
 
    def normal( self, mean, stdDev, size):
      return self.rng.normal( loc = mean , scale = stdDev , size = 10000 )
-   #    # if(self.seed is not None):
-   #    #    data_normal = norm.rvs ( size = 10000, loc = mean, scale = stdDev, random_state = self.seed )
-   #    # else:
-   #    #    data_normal = norm.rvs ( size = 10000, loc = mean, scale = stdDev )
-   #    # return data_normal
 
    def uniform( self, lowerBound , upperBound , size):
-     return self.rng.uniform( low = lowerBound , high = upperBound , size = 10000)
-   #    # if(self.seed is not None):
-   #    #    data_uniform = uniform.rvs ( size = 10000, loc = lowerBound, scale = upperBound, random_state = self.seed )
-   #    # else: 
-   #    #    data_uniform = uniform.rvs ( size = 10000, loc = lowerBound, scale = upperBound )
-   #    # return data_uniform
+     return self.rng.uniform( low = lowerBound , high = upperBound , size = 10000 )
 
-   # def uniformInt( self, lowerBound, upperBound ) :
-   #    return self.rng.randint( high = upperBound , low = lowerBound )
-   # #    # if(self.seed is not None):
-   # #    #    data_uniformInt = randint.rvs ( high = upperBound, low = lowerBound , loc = 0, size = 1000, random_state = self.seed )
-   # #    # else:
-   # #    #    data_uniformInt = randint.rvs ( high = upperBound, low = lowerBound , loc = 0, size = 1000)
-   # #    # return data_uniformInt
+   def uniformInt( self, lowerBound, upperBound ) :
+      return self.rng.randint( high = upperBound , low = lowerBound )
 
    def triangular( self, lowerBound, upperBound, mode , size ):
       return self.rng.triangular( left = lowerBound, mode = mode, right = upperBound , size = 10000 )
-   #  def triangular( self, lowerBound, upperBound, mode ):
-   # #    # if(self.seed is not None):
-   # #    #    data_triangular = triang.rvs ( size = 10000, c = mode, loc = lowerBound, scale = upperBound, random_state = self.seed )
-   # #    # else:
-   # #    #    data_triangular = triang.rvs ( size = 10000, c = mode, loc = lowerBound, scale = upperBound )
-   # #    # return data_triangular
 
    def pareto( self, shape , size ):
       return self.rng.pareto( a = shape , size = 10000)
-   # #    # if(self.seed is not None):
-   # #    #    data_pareto  = pareto.rvs ( size = 10000, b = shape, random_state = self.seed )
-   # #    # else:
-   # #    #    data_pareto  = pareto.rvs ( size = 10000, b = shape )
-   # #    # return data_pareto
+
    def weibull( self, shape , size ):
       return self.rng.weibull( a = shape , size = 10000 )
-
-   # #    # if(self.seed is not None):
-   # #    #    data_weibull = weibull_max.rvs( c = shape, scale = scale, size = 10000, random_state = self.seed  )
-   # #    # else:
-   # #    #    data_weibull = weibull_max.rvs( c = shape, scale = scale, size = 10000  )
-   # #    # return data_weibull
    
-   # def frequency( self, frequencyMap ):
-   # #    arrayOfkeys = []
-   #    weights = []
-   #    for key in frequencyMap:
-   #       arrayOfkeys.append( int(key) )
-   #       weights.append( frequencyMap[key] )
-   #    cumcout, lowerlimit, binsize, extrapoints = stats.cumfreq( arrayOfkeys, numbins = 4, weights = weights )
-   #    return cumcout
+   def frequency( self, frequencyMap ):
+      arrayOfkeys = []
+      weights = []
+      for key in frequencyMap:
+         arrayOfkeys.append( int( key) )
+         weights.append( frequencyMap[key] )
+      cumcout, lowerlimit, binsize, extrapoints = stats.cumfreq( arrayOfkeys, numbins = 10, weights = weights )
+      return cumcout
 
-   #  def pertdist( self, minimum, most_likely, maximum ):
-   # #    # pert = PERT( minimum, most_likely, maximum )
-   # #    # if(self.seed is not None):
-   # #    #    pert = sns.kdeplot( pert.rvs( 10000, random_state = self.seed ) )
-   # #    # else:
-   # #    #    pert = sns.kdeplot( pert.rvs( 10000) )
-
+   def pertdist( self, minimum, most_likely, maximum ):
+      pert = PERT( minimum, most_likely, maximum )
+      return pert
 
 if __name__ == "__main__":
    seed = 1234567  # test seed
    rand = Rand( seed )
-   # exponential_value = rand.exponential( 0.5 )
-   # print(exponential_value)
-   # gamma_value = rand.gamma( 1.0, 2.0  )
+   exponential_value = rand.exponential( 0.5, 10000 )
+   gamma_value = rand.gamma( 1.0, 2.0 , 1000)
    normal_value = rand.normal( 1.5, 0.5 , 10000)
-   print(normal_value)
    uniform_value = rand.uniform( 0.5, 1.5 , 10000 )
-   # uniformInt_value = rand .uniformInt( 1, 40 )
-   triangular_value = rand.triangular( 0.5, 1.5, 1.0 , 10000)
-   pareto_value = rand.pareto( 2.0 , 10000)
-   weibull_value = rand.weibull( 0.5 , 10000)
-   print( weibull_value )
-   # frequency_value = rnd.frequency( { "20":1.4, "30":2.6 , "60":3.8, "80":1.9 } )
-   # pert_value = rnd.pertdist( 10, 190, 200 )
+   # uniformInt_value = rand.uniformInt( 1, 40 )
+   triangular_value = rand.triangular( 0.5, 1.5, 1.0 , 1000 )
+   pareto_value = rand.pareto( 2.0 , 10000 )
+   weibull_value = rand.weibull( 0.5 , 10000 )
+   frequency_value = rand.frequency( { "20":1.4, "30":2.6 , "60":3.8, "80":1.9 } )
+   pert_value = rand.pertdist( 10, 190, 200 )
   
 
-   # make_plot("Exponential", exponential_value)
-   # make_plot( "Gamma", gamma_value )
+   plots.plot_exponential( "Exponential", exponential_value )
+   plots.plot_gamma( "Gamma", gamma_value , 1.0 , 2.0 )
    plots.plot_normal( "Normal", normal_value , 1.5, 0.5 )
    plots.plot_uniform( "Uniform", uniform_value )
-   # make_plot( "UniformInt", uniformInt_value )
+   # plots.plot_uniformInt( "UniformInt", uniformInt_value )
    plots.plot_triangle( "Triangular", triangular_value  )
    plots.plot_pareto( "Pareto", pareto_value , 2.0 ,0 )
-   plots.plot_weibull( "Weibull", weibull_value )
-   
-   # make_plot( "Frequency", frequency_value )
-   # make_plot( "PertDistributiion", pert_value )
+   plots.plot_weibull( "Weibull", weibull_value  )
+   plots.plot_frequency( "Frequency", frequency_value )
+   plots.plot_pert( "PertDistributiion", pert_value , 10 , 190 , 200 )
--- Python/Core1/lib/plots.py
+++ Python/Core1/lib/plots.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
+import scipy.special as sps
 if not sys.warnoptions:
     import warnings
     warnings.simplefilter( "ignore" )
@@ -11,28 +12,61 @@ sns.set( color_codes = True )
 # settings for seaborn plot sizes
 sns.set( rc = {'figure.figsize':(5,5)} )
 
-def plot_normal(name, data, mu, sigma ):
-	count, bins, ignored = plt.hist(data, 30, density=True)
-	plt.plot( bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r' )
+def plot_exponential( name, data ):
+	count, bins, ignored = plt.hist(data, 15, density = True)
+	plt.plot( linewidth = 2, color = 'r' )
+	plt.title( "Exponential" ) 
 	plt.show()
-def plot_triangle( name, data ):
-	h = plt.hist( data, bins=200, density=True , rwidth = 3, color='r' )
+
+def plot_gamma ( name, data ,shape , scale ) :
+	count, bins, ignored = plt.hist( data, 15, density = True ) 
+	y = bins**( shape-1 )*( np.exp( -bins/scale ) /  ( sps.gamma( shape )*scale**shape ) )
+	plt.plot( bins, y, linewidth = 2, color = 'r' )
+	plt.title( "Gamma" )
+	plt.show()
+
+def plot_normal(name, data, mu, sigma ) :
+	count, bins, ignored = plt.hist(data, 15, density = True)
+	plt.plot( bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth = 2, color = 'r' )
+	plt.title ( "Normal" )
+	plt.show()
+def plot_triangle( name, data ) :
+	h = plt.hist( data, bins = 20, density = True , rwidth = 3, color ='skyblue' )
+	plt.title( "Triangular" )
 	plt.show()
 def plot_uniform( name, data ) :
-	count, bins, ignored = plt.hist(data, 15, density=True)
-	plt.plot(bins, np.ones_like(bins), linewidth=2, color='r')
+	count, bins, ignored = plt.hist( data, 15, density = True )
+	plt.plot(bins, np.ones_like( bins ), linewidth = 2, color = 'r')
+	plt.title( " Uniform" )
+	plt.show()
+def plot_uniformInt( name, data ) :
+	count, bins, ignored = plt.hist( data, 15, density = True )
+	plt.plot(bins, np.ones_like( bins ), linewidth = 2, color = 'r')
+	plt.title ( "UniformInt" )
 	plt.show()
 def plot_pareto( name , data , a , m ) :
-	count, bins, _ = plt.hist( data, 100, density=True )
+	count, bins, _ = plt.hist( data, 15, density = True )
 	fit = a*m**a / bins**(a+1)
-	plt.plot( bins, max( count )*fit/max( fit ), linewidth=2, color='r' )
+	plt.plot( bins, max( count )*fit/max( fit ), linewidth = 2, color = 'r' )
+	plt.title ( "Pareto" )
 	plt.show()
-def plot_weibull(name , data  ):
-	count, bins, _= plt.hist(data, density = True)
+def plot_weibull( name , data  ) :
+	count, bins, _= plt.hist( data, 15, density = True )
 	# x = np.arange(1,100.)/50.
 	# scale = count.max()/plot_weibull(a, 1., 5.).max()
 	# plt.plot(x, plot_weibull(x, 1., 5.)*scale)
+	plt.title ( "weibull" )
 	plt.show()
+def plot_pert( name, data ):
+	sns.kdeplot(data.rvs(10000))
+	plt.title("Pert")
+	plt.show()
+
+def plot_frequency( name, data ):
+	count, bins, ignored = plt.hist(data, 15, linewidth = 2 ,density = True) 
+	plt.title ( "Frequency" )
+	plt.show()
+
 # def make_plot( name, data ):
 # 	count, bins, ignored = plt.hist(data, 30, density=True)
 # 	plt.plot(bins, data, linewidth=2, color='r')
